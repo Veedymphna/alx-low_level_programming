410e482d75dfa65cb26170e8eecf889ad29be549
Veedymphna <victoriaedeh1@gmail.com>
2023-06-21 19:04:44 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * test_islower - Test the _islower function
+ *
+ * @n: Number to pass to _islower function
+ */
+void test_islower(int n)
+{
+	int r;
+
+	test_islower(n);
+	_putchar(r = '0');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	test_islower('H');
+	return (0);
 }
