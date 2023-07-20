a0af16febcc061ce7b5faa0f6e351f4f2a7218dd
Veedymphna <victoriaedeh1@gmail.com>
2023-07-19 17:17:47 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,6 +5,7 @@
  * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
